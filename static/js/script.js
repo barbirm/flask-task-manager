a302cc0513842dee5c69aa64da3faa936ca162fd
MateBar <barbirm@mail.com>
2021-07-29 16:08:22 +0100

    Bind data from database to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -8,4 +8,7 @@ $(document).ready(function () {
     $(document).ready(function () {
         $('.collapsible').collapsible();
     });
+    $(document).ready(function () {
+        $('.tooltipped').tooltip();
+    });
 });
